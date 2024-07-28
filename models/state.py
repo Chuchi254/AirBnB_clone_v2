e82b2475f892233176fb3c95b48b0afe391f9d37
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-28 05:31:20 -0700

    improve engines

--- models/state.py
+++ models/state.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models import storage
+from models.city import City
 
 
 class State(BaseModel):
     """ State class """
     name = ""
+
+    @property
+    def cities(self):
+        """Get a list of all related City objects"""
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list

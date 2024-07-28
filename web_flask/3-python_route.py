df5e163ef1d8d407ca6d15ed77c67434aa7a8483
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-28 01:53:36 -0700

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application with four routes.
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Display 'Hello HBNB!' when accessing the root route.
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Display 'HBNB' when accessing the /HBNB route.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Display 'C ' followed by the value of the text variable, with
+    underscores replaced by splaces.
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """
+    Display 'Python ' followed by the value of the text variable, with
+    underscores replaced by spaces. The default value is value is 'is cool'
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    """
+    Run the Flask web application.
+    """
+    app.run(host="0.0.0.0", port=5000)
